161adb01c1c8cb0df07e2fc840c333c53da07cdf
Zuki <530276014@qq.com>
2020-05-20 15:06:35 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+function stitch() {
+
+}
+
+function handleUpload(files) {
+    // console.log(files)
+    const pics = document.getElementById("pics");
+
+    // let height=0, width=0;
+    for (let i = 0; i < files.length; i++) {
+        const img = files[i];
+        // console.log(img);
+        const reader = new FileReader();
+        reader.readAsDataURL(img);
+        // console.log(reader);
+        reader.onload = e => {
+            // console.log("DONE")
+            const imgURL = reader.result;
+            const imgEl = document.createElement("img");
+            imgEl.src = imgURL;
+            pics.appendChild(imgEl);
+        }
+
+    }
+}
+
+function drawPic() {    
+    const pics = document.getElementById("pics").children;
+    let height = 0, width = 0, maxHeight = 0, maxWidth = 0;
+    for(let i = 0;i<pics.length; i++) {
+        height += pics[i].naturalHeight;
+        width += pics[i].naturalWidth;
+    }
+    
+    const canvas = document.getElementById("canvas");
+    canvas.width = width;
+    canvas.height = height;
+    const context = canvas.getContext("2d");
+    for(let i = 0,x=0,y=0;i<pics.length; i++) {
+        context.drawImage(pics[i],x,y);
+        // height += pics[i].naturalHeight;
+        // width += pics[i].naturalWidth;
+        x += pics[i].naturalWidth;
+    }
+}
